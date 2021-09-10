61b6af6918240e443e32224f94537251ca4e3a6a
vansari <junker.kurt@googlemail.com>
2021-09-10 12:17:51 +0200

    First initial commit for Bundle

---
+++ src/FreesoftdeLiquibaseBundle.php
@@ -0,0 +1,10 @@
+<?php
+declare(strict_types = 1);
+
+namespace Freesoftde\LiquibaseBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class FreesoftdeLiquibaseBundle extends Bundle {
+
+}
